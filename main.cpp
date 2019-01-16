b077f43211c3f7aeb1adc0c98acbaac64431b382
Tygydyk <32385649+Tygydyk@users.noreply.github.com>
2019-01-16 10:34:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,20 @@ const char * const json_data = R"({
         "street" : "Vozdvijenka"
     }
 })";
+    
+     Json object = Json::parse(json_data);
+    cout<<std::any_cast<std::string>(object["lastname"])<<endl;
+    cout<<std::any_cast<bool>(object["islegal"])<<endl;
+    cout<<std::any_cast<int>(object["age"])<<endl;
+
+    auto marks = std::any_cast<Json>(object["marks"]);
+    cout<<std::any_cast<int>(marks[0])<<endl;
+    cout<<std::any_cast<int>(marks[1])<<endl;
+
+    auto address = std::any_cast<Json>(object["address"]);
+    cout<<std::any_cast<std::string>(address["city"])<<endl;
+    cout<<std::any_cast<std::string>(address["street"])<<endl;
+
 
 return 0;
 }
